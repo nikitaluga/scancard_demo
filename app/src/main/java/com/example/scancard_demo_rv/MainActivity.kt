68005f0491a217285366570a07bf993a2021c770
RENDEZ-VOUS\57533 <n.lugovoy@rendez-vous.ru>
2022-05-05 14:44:51 +0300

    initial commit

---
+++ app/src/main/java/com/example/scancard_demo_rv/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.scancard_demo_rv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
